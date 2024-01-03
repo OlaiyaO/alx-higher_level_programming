aa2a25169ef85688cb0ec3e0915ea583def8a5eb
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-03 14:40:28 +0000

    File: 11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+
+    Args:
+    - a: a number (integer or float)
+    - b: an integer
+
+    Returns:
+    - The value of a raised to the power of b
+    """
+    return a ** b
