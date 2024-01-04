0d501e5409ba97ba9c9bf62a1ff2841409673493
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-04 15:39:55 +0000

    Made and edit to File: 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def magic_calculation(a, b):
-        from calculation_102 import add, sub
-
+def magic_calculation(a, b):
     if a < b:
-        result = add(a, b)
+        c = a + b
         for i in range(4, 6):
-            result = add(result, i)
-        return result
+            c = c + i
+        return c
     else:
-        return sub(a, b)
+        return a - b
