05172a660146110eaf10351d769aa27132cb5274
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-04 15:05:53 +0000

    Fixed indentation and tested with pycode style File: 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,20 +2,20 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-import sys
+    import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-operators = {"+": add, "-": sub, "*": mul, "/": div}
-operator = sys.argv[2]
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = sys.argv[2]
 
-if operator not in operators:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-a, b = map(int, (sys.argv[1], sys.argv[3]))
-result = operators[operator](a, b)
+    a, b = map(int, (sys.argv[1], sys.argv[3]))
+    result = operators[operator](a, b)
 
-print(f"{a} {operator} {b} = {result}")
+    print(f"{a} {operator} {b} = {result}")
