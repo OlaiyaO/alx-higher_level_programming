d0ce040a156b1179f36ad08ed511a8ac1fb7f45d
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-06 10:25:44 +0000

    rewrote File: 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for number in row:
-            print("{:d}".format(number), end=" ")
+        for i, number in enumerate(row):
+            print("{:d}".format(number), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
         print()
