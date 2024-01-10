a7a448787069983e1caefb5aa7d355b2085a1fbc
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-10 16:04:39 +0000

    File: 103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,58 @@
+#include <Python.h>
+
+void print_python_bytes(PyObject *p) {
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p)) {
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	Py_ssize_t size = PyBytes_Size(p);
+	char *str = PyBytes_AsString(p);
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+
+	printf("  first 10 bytes: ");
+	for (Py_ssize_t i = 0; i < size && i < 10; ++i) {
+		printf("%02x ", (unsigned char)str[i]);
+	}
+	printf("\n");
+}
+
+void print_python_list(PyObject *p) {
+	printf("[*] Python list info\n");
+
+	if (!PyList_Check(p)) {
+		printf("[ERROR] Invalid Python List Object\n");
+		return;
+	}
+
+	Py_ssize_t size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (Py_ssize_t i = 0; i < size; ++i) {
+		PyObject *elem = PyList_GetItem(p, i);
+		printf("Element %ld: ", i);
+
+		if (PyBytes_Check(elem)) {
+			print_python_bytes(elem);
+		} else if (PyLong_Check(elem)) {
+			printf("int\n");
+		} else if (PyFloat_Check(elem)) {
+			printf("float\n");
+		} else if (PyTuple_Check(elem)) {
+			printf("tuple\n");
+		} else if (PyList_Check(elem)) {
+			printf("list\n");
+		} else if (PyUnicode_Check(elem)) {
+			printf("str\n");
+		} else {
+			printf("unknown type\n");
+		}
+	}
+}
+
