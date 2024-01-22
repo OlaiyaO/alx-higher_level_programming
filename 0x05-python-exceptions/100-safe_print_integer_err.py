62858aae5ee8ad7290be3ae56debae67283d23d9
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-22 18:18:21 +0100

    File: 100-safe_print_integer_err.py once more

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except (ValueError, TypeError):
-        exception_info = sys.exc_info()[1]
-        print("Exception: {}".format(exception_info), file=sys.stderr)
-        return (False)
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr)
+        return False
