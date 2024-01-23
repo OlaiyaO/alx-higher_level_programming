3f4c307b0787ea5eaca00bf0481ffa90d2b4666f
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-23 20:37:18 +0100

    File: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,8 +82,9 @@ class Square:
         if self.__size == 0:
             print("")
             return
-
-        print("\n" * self.__position[1], end="")
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
