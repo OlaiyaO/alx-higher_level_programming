44ea8b5bb71660f327f27b75ef915f2fc38db5fd
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-23 15:43:31 +0100

    Solving File: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
