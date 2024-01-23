15eb30187bae85ff6adf69eacf91b5be1eb9921e
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-23 15:46:36 +0100

    Solving File: 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
