bb60ff7fd582f403e8e290d2d3cbab88adb91c4e
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-24 04:50:38 +0100

    File: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,7 +65,8 @@ class Square:
             TypeError: If value is not a tuple of 2 positive integers.
         """
         if not isinstance(value, tuple) or len(value) != 2 \
-                or not all(isinstance(i, int) and i >= 0 for i in value):
+                or not all(isinstance(i, int) and i >= 0 for i in value) \
+                or not all(num >= 0 for num in value):
                     raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -82,9 +83,8 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
