c24f1279a29c02c85afcd406418c7524e0242ea9
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-30 15:08:20 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""Module containing LockedClass."""
+
+
 class LockedClass:
+    """
+    A class that prevents the dynamic creation of new instance attributes,
+    except for 'first_name'.
+
+    Attributes:
+        first_name (str): The allowed instance attribute.
+    """
+
     __slots__ = ('first_name',)
