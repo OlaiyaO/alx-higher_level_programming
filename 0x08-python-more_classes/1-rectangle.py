2c48a186f3378e3cf7746cde77b2587ee06347bc
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-30 04:06:46 +0100

    Trying 1-main.py again

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -40,7 +40,7 @@ class Rectangle:
         """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
