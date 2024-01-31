9461cc696adc040aa1cf9cb799ed02432111abf8
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-01-31 04:54:25 +0100

    I'm simply holding my breath for the result of today's work

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Definition for a function that divides matrices."""
+
 
 def matrix_divided(matrix, div):
     """
@@ -16,20 +18,13 @@ def matrix_divided(matrix, div):
                    or div is equal to 0.
         ValueError: If rows of the matrix have different sizes.
     """
-    if not all(isinstance(row, list) and all(isinstance(el, (int, float)) for el in row) for row in matrix):
+    if (not all(isinstance(row, list) and all(isinstance(el, (int, float))
+        for el in row) for row in matrix)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise ValueError("Each row of the matrix must have the same size")
-
     return [[round(el / div, 2) for el in row] for row in matrix]
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Formats text with line breaks; raises TypeError for non-strings."""
+
 
 def text_indentation(text):
     """
