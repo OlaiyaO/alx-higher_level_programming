be7d3d9c4a41bd352dde7be72f0938c09b0bf6a8
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-05 19:05:50 +0100

    File: 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a function is_kind_of_class(obj, a_class)."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True for the specified class; otherwise False."""
+    return isinstance(obj, a_class)
