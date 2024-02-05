3a7315eb2a6d34ba28fa4165458a7908173ce04b
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-05 20:56:32 +0100

    Fixed indentation error

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initializes a Square instance with size."""
-       self.integer_validator("size", size)
-       super().__init__(size, size)
-       self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         """Returns a string representation of the square."""
