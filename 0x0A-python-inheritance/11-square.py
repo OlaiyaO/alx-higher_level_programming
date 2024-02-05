b08636b257929fb8cc9f3538c31ebf774cb2da7f
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-05 20:10:15 +0100

    File: 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Extends Square with a customized string representation."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectangle and represents a square."""
+
+    def __init__(self, size):
+        """Initializes a Square instance with size."""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns a string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
