d3895bd794be2d9f05b1fa79f69d209f1f63480a
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-05 20:43:22 +0100

    trying to fix 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Defines a class Square that inherits from Rectangle with validated props"""
-
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -13,7 +11,3 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def __str__(self):
-        """Returns a string representation of the square."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
