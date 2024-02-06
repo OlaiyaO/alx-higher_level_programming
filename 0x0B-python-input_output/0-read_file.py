158bc97a7d5922a0a4536b442681e71d0b7cd1e9
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-06 12:07:04 +0100

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+README.md file for 0x0B-python-input_output
