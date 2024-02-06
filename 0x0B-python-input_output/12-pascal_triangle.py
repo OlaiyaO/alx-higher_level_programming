b0718a67827d1d132878f4f3fe9524ead50386c3
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-06 18:23:32 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Creates a Pascal's triangle of a given size.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Creates a Pascal's triangle of a given size.
+
+    Args:
+        n (int): The number of rows in the Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing the Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
