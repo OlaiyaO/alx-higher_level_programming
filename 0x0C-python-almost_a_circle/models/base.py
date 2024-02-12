26cf77cdeaad3b2117fb6902ce6e391fc296a1b5
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-12 15:23:45 +0100

    to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts dictionary to JSON string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
