f1c663fb43f8b8abba0310a34ccfa3282ea03111
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-12 15:32:36 +0100

    Added static method from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,21 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                obj_list = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(obj_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Converts JSON string to list of dictionaries"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
