71c78c89e7f36aa3e7dae7709c5431fba85c7f93
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-12 17:05:22 +0100

    File: 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- Assuming the table dump has been imported into hbtn_0c_0 database
 
 -- Calculate the max temperature by state and display it ordered by state name
-SELECT state, MAX(temperature) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
