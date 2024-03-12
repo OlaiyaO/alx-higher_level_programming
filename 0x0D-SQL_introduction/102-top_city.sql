36bfe3e02bc43aa0e6e15fe608bc2e4deb11be94
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-12 16:59:39 +0100

    Edited the top_city file

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 -- Calculate the average temperature during July and August by city and display the top 3 cities ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
