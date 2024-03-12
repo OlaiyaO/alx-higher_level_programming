4b7e4a6535df107ff14cc8272da13bfb388b6ee1
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-12 16:41:30 +0100

    Add all

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Assuming the table dump has been imported into hbtn_0c_0 database
+
+-- Calculate the average temperature during July and August by city and display the top 3 cities ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE MONTH(date) IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
