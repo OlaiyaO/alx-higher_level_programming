0e8ba0ae9c7c7a913841b06ffd4bfa622c9741ae
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-13 22:18:30 +0100

    Let'ss hope it finishes

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,7 @@
 -- Selecting genre names and their corresponding total rating
--- Lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT name, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
 ORDER BY rating_sum DESC;
