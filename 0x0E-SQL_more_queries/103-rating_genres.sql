2ddc84259d67c0279b84967f9801dc18734ee569
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-13 22:24:24 +0100

    Removed the sum

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- Selecting genre names and their corresponding total rating
-SELECT name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
