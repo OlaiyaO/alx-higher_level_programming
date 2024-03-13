d05af5f852a05f0a4a2b22290b61827ae9d2aff0
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-13 15:24:12 +0100

    2. Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,3 @@
+-- Create MySQL user user_0d_2 with only SELECT privilege on hbtn_0d_2 database
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
