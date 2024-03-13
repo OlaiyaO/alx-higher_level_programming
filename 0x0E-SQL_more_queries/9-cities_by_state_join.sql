0f2cb3d84bbf8acdfaf72c02cdc7097982b84005
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-13 18:01:46 +0100

    Used the Join statement

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- 9-cities_by_state_join.sql
+
+-- Query to list all cities along with their corresponding states using JOIN
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
