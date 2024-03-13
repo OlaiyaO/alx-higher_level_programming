3833d922ffab8a9cd320b6f4d2122b64e226fee4
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-13 19:26:13 +0100

    Done some edits

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Select show titles and sum of ratings, ordering by rating sum in descending order
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
