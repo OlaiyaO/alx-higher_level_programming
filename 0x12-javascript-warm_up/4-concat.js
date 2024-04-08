2af525842aef3931966baa99796441a158a43488
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-08 14:27:34 +0100

    Added semistandard

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log(process.argv[2] + " is undefined");
+  console.log(process.argv[2] + ' is undefined');
 } else {
-  console.log(process.argv[2] + " is " + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
