02f8c2ac279f6a2c850c1b7cea4a1cb3beeb3598
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-08 15:26:31 +0100

    Infinity Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 function factorial (num) {
-  if (num === 0 || num === 1) {
+  if (isNaN(num)) {
+    return 1;
+  } else if (num === 0 || num === 1) {
     return 1;
   } else {
     return num * factorial(num - 1);
