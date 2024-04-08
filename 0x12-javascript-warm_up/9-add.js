9847a068b36f86a4540f1f8f858b8335ddc504f0
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-08 15:21:04 +0100

    Fix semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,6 +2,10 @@
 function add (a, b) {
   return a + b;
 }
-module.exports = {
-  add
-};
+if (process.argv.length !== 4) {
+  console.log('NaN');
+} else {
+  const num1 = parseInt(process.argv[2]);
+  const num2 = parseInt(process.argv[3]);
+  console.log(add(num1, num2));
+}
