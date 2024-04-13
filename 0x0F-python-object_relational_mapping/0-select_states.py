30a1a5b2355cb2a9eb7c64b402bca52db2f1bd4d
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-13 09:05:50 +0100

    Further work on )-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+def main():
+    """
+    Main function to connect to MySQL database and retrieve states
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )
+
+    except MySQLdb.Error as e:
+        print("MySQLdb Error:", e)
+        sys.exit(1)
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
