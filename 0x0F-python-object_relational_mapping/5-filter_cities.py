b7050002411c1a05c52ddfff5584da4085796807
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-13 15:15:22 +0100

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,43 +6,26 @@ import MySQLdb
 import sys
 
 
-def main():
-    """
-    Main function to connect to MySQL database and list cities by state
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    try:
-        with MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        ) as db:
-
-            with db.cursor() as cursor:
-                query = """
-                    SELECT cities.id, cities.name
-                    FROM cities
-                    JOIN states ON cities.state_id = states.id
-                    WHERE states.name = %s
-                    ORDER BY cities.id ASC
-                """
-                cursor.execute(query, (state_name,))
-
-                cities = cursor.fetchall()
-
-                if cities:
-                    print(", ".join([city[1] for city in cities]))
-
-    except MySQLdb.Error as e:
-        print("MySQLdb Error:", e)
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    main()
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.name FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC", (sys.argv[4],))
+
+    cities = cursor.fetchall()
+
+    print(", ".join(city[0] for city in cities))
+
+    cursor.close()
+    db.close()
