5febab699ca4e6ad32c2c6d2e2d2604468b754d6
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-13 11:50:13 +0100

    Worked on 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main function to connect to MySQL database and filter states by user input
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as e:
+        print("MySQLdb Error:", e)
+        sys.exit(1)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
