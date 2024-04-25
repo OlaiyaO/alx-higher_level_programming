31926eea4a65ba8ab79983a8593565551a15d256
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-25 12:25:01 +0100

    answered most of the questions

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request, and displays the body of the response (for status code 200)
-
+# This script takes a URL, sends a GET request.
 curl -sL "$1"
