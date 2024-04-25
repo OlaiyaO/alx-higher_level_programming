2488d912035b6d2672dbea49f34a552e4136c060
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-25 12:28:06 +0100

    answered most of the questions

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request with a custom header, and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# This script takes a URL, sends a GET request with a custom header.
+curl -s -H "X-School-User-Id: 98" "$1"
