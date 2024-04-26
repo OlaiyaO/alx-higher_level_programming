2128c3353962bdbb63f38a14aace5e9164bb6d83
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-26 11:58:13 +0100

    solved pycodestyle errors

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,8 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    response = requests.get('https://api.github.com/user', auth=(username, password))
+    response = requests.get(
+            'https://api.github.com/user', auth=(username, password))
     try:
         data = response.json()
         print(data.get('id'))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email address, sends a POST request to the passed URL 
+Takes in a URL and an email address, sends a POST request to the passed URL
 with the email as a parameter, and finally displays the body of the response.
 """
 import requests
