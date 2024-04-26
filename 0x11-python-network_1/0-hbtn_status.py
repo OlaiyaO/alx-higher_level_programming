e0ae54c4058798e7fcd35b5eea449ace6be847e1
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-26 11:34:06 +0100

    working on the sandbox now

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
