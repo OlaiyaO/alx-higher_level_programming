8c80e7a4812c92fdf8f11ea384898b7ee6c2c2a7
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-21 13:42:56 +0100

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected response:', response.statusCode);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+
+  const count = films.filter(film =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  ).length;
+
+  console.log(count);
+});
