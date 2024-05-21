c02f187e1cb6e6b6a6cf3c8b51d855c56828e17e
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-21 13:25:31 +0100

    Use of API's

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+const movieId = process.argv[2]
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error)
+    return
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error code: ' + response.statusCode)
+    return
+  }
+
+  const film = JSON.parse(body)
+
+  console.log(film.title)
+})
