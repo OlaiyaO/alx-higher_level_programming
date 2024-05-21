ec88cb079a744eedaef7d2543f5c96d3aea7b48b
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-21 12:45:06 +0100

    Working on some terminal things

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,9 +6,5 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`The content was successfully written to ${filePath}`);
-  }
+  if (err) console.log(err);
 });
