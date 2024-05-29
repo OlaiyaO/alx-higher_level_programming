3196689aa401b43ec65306527c3be171b4169b1c
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-29 13:52:37 +0100

    Working on some jquery scripts

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  const films = data.results;
+  films.forEach(film => {
+    $('#list_movies').append('<li>' + film.title + '</li>');
+  });
+});
