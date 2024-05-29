cff7baa992138a86530f22c602b615773a08c5d9
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-29 16:06:43 +0100

    worekd on some scripts

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  function fetchHello () {
+    const langCode = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`, function (data) {
+      $('#hello').text(data.hello);
+    });
+  }
+
+  $('#btn_translate').click(fetchHello);
+
+  $('#language_code').keypress(function (event) {
+    if (event.which == 13) { // 13 is the Enter key code
+      fetchHello();
+    }
+  });
+});
